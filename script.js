87427eb7b67fe270521e8b3e3639d9eca6740345
turtleman9000 <49375077+turtleman9000@users.noreply.github.com>
2020-01-26 11:39:53 -0800

    add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  resize: true
+});
+
+
+
+
+
+
+
+
